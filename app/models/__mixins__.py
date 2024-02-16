d0c6e63de5c5a2135487ac288ec87121679ad937
David Carmichael <carmichaelits@gmail.com>
2024-02-16 07:00:33 +0000

    feat: move private methods to bottom of class

--- app/models/__mixins__.py
+++ app/models/__mixins__.py
@@ -50,183 +50,6 @@ class ModelAttributeError(Exception):
 class UtilityMixin:
     __fsa__: SQLAlchemy
 
-    @classmethod
-    def _um_check_fsa_exists(cls):
-        if not hasattr(cls, "__fsa__"):
-            raise AttributeError(
-                dedent(
-                    """
-                The class using this mixin must have a __fsa__ attribute set to the Flask-SQLAlchemy instance",
-                Example:
-                from app.extensions import db
-                class Example(db.Model, UtilityMixin):
-                    __fsa__ = db
-                """
-                ),
-            )
-
-    @staticmethod
-    def _um_row_as_dict(row: Row) -> dict:
-        return {key: row.__dict__[key] for key in row.__dict__ if key[0] != "_"}
-
-    @staticmethod
-    def _um_parse_value(key, value, type_):
-        """
-        Returns value as type_ if possible, otherwise raises ParseValueException
-
-        :param key:
-        :param value:
-        :param type_:
-        :return:
-        """
-
-        if isinstance(type_, sqltypes.DateTime):
-            if isinstance(value, dt):
-                return value
-
-            if isinstance(value, d):
-                return dt.combine(value, dt.min.time())
-
-            if isinstance(value, str):
-                # Guess the format
-                try:
-                    return dt.strptime(value, "%Y-%m-%d")
-                except ValueError:
-                    pass
-
-                try:
-                    return dt.strptime(value, "%Y-%m-%dT%H:%M:%S")
-                except ValueError:
-                    pass
-
-                try:
-                    return dt.strptime(value, "%Y-%m-%dT%H:%M:%d.%f")
-                except ValueError:
-                    pass
-
-                raise ParseValueError(key, value, type_)
-
-        if isinstance(type_, sqltypes.Integer):
-            try:
-                return int(value)
-            except ValueError:
-                pass
-
-            raise ParseValueError(key, value, type_)
-
-        if isinstance(type_, sqltypes.Boolean):
-            if isinstance(value, bool):
-                return value
-            if isinstance(value, str):
-                if value.lower() in ["yes", "true", "t", "1"]:
-                    return True
-                return False
-            if isinstance(value, int):
-                if value == 1:
-                    return True
-                return False
-
-            raise ParseValueError(key, value, type_)
-
-        return value
-
-    @classmethod
-    def _parse_rows(
-        cls,
-        rows: t.Union[Row, list, dict],
-        include_joins: list[tuple[str, str] | str] = None,
-        cast_joins: list[tuple[str, str]] = None,
-        all_columns_but: list = None,
-        only_columns: list = None,
-        _is_join: bool = False,
-    ) -> t.Union[dict, list]:
-        """
-        Parses the rows into a dictionary or list of dictionaries
-
-        include_joins: [('given_name', 'join_attr') | 'join_attr']
-        cast_joins: [('given_name', 'join_attr.attr_of_join')]
-
-        all_columns_but: ['column_name', ...] # exclude these columns in the return
-        only_columns: ['column_name', ...] # only include these columns in the return
-
-        _is_join: Tell the function if it is parsing a join or not, this is for
-        internal use only
-        """
-
-        if isinstance(rows, list):
-            return [cls._parse_rows(row, _is_join=True) for row in rows]
-
-        if include_joins is None:
-            include_joins = []
-
-        if all_columns_but is None:
-            all_columns_but = []
-
-        if only_columns is None:
-            only_columns = []
-
-        def include_column(column_: str) -> bool:
-            if only_columns:
-                if column_ not in only_columns:
-                    return False
-
-            if all_columns_but:
-                if column_ in all_columns_but:
-                    return False
-
-            return True
-
-        data = dict()
-        for column, value in cls._um_row_as_dict(rows).items():
-            if not _is_join:
-                if not include_column(column):
-                    continue
-
-            if isinstance(value, collections.InstrumentedList):
-                continue
-
-            data[column] = value
-
-        if _is_join:
-            return data
-
-        joins = dict()
-        if include_joins:
-            for join in include_joins:
-                if isinstance(join, tuple):
-                    name, join_attr = join
-                    if hasattr(rows, join_attr):
-                        joins[name] = [
-                            cls._parse_rows(row, _is_join=True)
-                            for row in getattr(rows, join_attr)
-                        ] or []
-
-                if isinstance(join, str):
-                    if hasattr(rows, join):
-                        if isinstance(getattr(rows, join), list):
-                            joins[join] = [
-                                cls._parse_rows(row, _is_join=True)
-                                for row in getattr(rows, join)
-                            ] or []
-
-                            continue
-
-                        joins[join] = cls._parse_rows(rows, _is_join=True)
-
-        if cast_joins:
-            for given_name, join_cast in cast_joins:
-                split_join_cast = join_cast.split(".")
-                if hasattr(rows, split_join_cast[0]):
-                    join_attr = getattr(rows, split_join_cast[0])
-                    if hasattr(join_attr, split_join_cast[1]):
-                        data[given_name] = getattr(join_attr, split_join_cast[1])
-                    else:
-                        data[given_name] = None
-                else:
-                    data[given_name] = None
-
-        return {**data, **joins}
-
     def um_commit(self):
         self._um_check_fsa_exists()
 
@@ -235,9 +58,9 @@ class UtilityMixin:
 
     @classmethod
     def um_count(
-        cls,
-        pkv: int = None,
-        fields: dict = None,
+            cls,
+            pkv: int = None,
+            fields: dict = None,
     ) -> int:
         """
         pkv is the primary key value
@@ -264,11 +87,11 @@ class UtilityMixin:
     # Create
     @classmethod
     def um_create(
-        cls,
-        values: dict,
-        allow_none: bool = True,
-        return_record: bool = False,
-        custom_insert: Insert = None,
+            cls,
+            values: dict,
+            allow_none: bool = True,
+            return_record: bool = False,
+            custom_insert: Insert = None,
     ) -> t.Optional[Result]:
         """
         values: {'model_attribute': 'value', ...}
@@ -303,11 +126,11 @@ class UtilityMixin:
 
     @classmethod
     def um_create_batch(
-        cls,
-        batch_values: list[dict],
-        allow_none: bool = True,
-        return_records: bool = False,
-        custom_insert: Insert = None,
+            cls,
+            batch_values: list[dict],
+            allow_none: bool = True,
+            return_records: bool = False,
+            custom_insert: Insert = None,
     ) -> t.Optional[list[Result | None]]:
         cls._um_check_fsa_exists()
 
@@ -338,24 +161,24 @@ class UtilityMixin:
     # Read
     @classmethod
     def um_read(
-        cls,
-        pkv: int = None,
-        fields: dict = None,
-        order_by: dict = None,
-        one_or_none: bool = False,
-        first: bool = False,
-        paginate: bool = False,
-        paginate_page: int = 1,
-        paginate_per_page: int = 10,
-        paginate_max_per_page: int = 100,
-        paginate_error_out: bool = True,
-        paginate_count: bool = True,
-        as_json: bool = False,
-        json_include_joins: list[tuple[str, str] | str] = None,
-        json_return_key_name: str = None,
-        json_only_columns: list = None,
-        json_remove_return_key: bool = False,
-        custom_select: Select = None,
+            cls,
+            pkv: int = None,
+            fields: dict = None,
+            order_by: dict = None,
+            one_or_none: bool = False,
+            first: bool = False,
+            paginate: bool = False,
+            paginate_page: int = 1,
+            paginate_per_page: int = 10,
+            paginate_max_per_page: int = 100,
+            paginate_error_out: bool = True,
+            paginate_count: bool = True,
+            as_json: bool = False,
+            json_include_joins: list[tuple[str, str] | str] = None,
+            json_return_key_name: str = None,
+            json_only_columns: list = None,
+            json_remove_return_key: bool = False,
+            custom_select: Select = None,
     ) -> t.Optional[t.Union[dict, list[t.Optional[Result]], Pagination]]:
         """
         pkv is the primary key value
@@ -438,15 +261,15 @@ class UtilityMixin:
     # Update
     @classmethod
     def um_update(
-        cls,
-        values: dict = None,
-        where: dict[str, t.Union[str, list]] = None,
-        skip_attrs: list[str] = None,
-        fail_on_unknown_attr: bool = True,
-        return_record: bool = False,
-        return_input_values: bool = False,
-        prevent_commit: bool = False,
-        custom_update: Update = None,
+            cls,
+            values: dict = None,
+            where: dict[str, t.Union[str, list]] = None,
+            skip_attrs: list[str] = None,
+            fail_on_unknown_attr: bool = True,
+            return_record: bool = False,
+            return_input_values: bool = False,
+            prevent_commit: bool = False,
+            custom_update: Update = None,
     ) -> t.Optional[t.Union[dict, Result]]:
         """
         where: {'model_attribute': 'value' | ['values'...], ...} :raw-html:`<br />`
@@ -514,10 +337,10 @@ class UtilityMixin:
             return iv
 
     def um_update_inline(
-        self,
-        values: dict,
-        fail_on_unknown_attr: bool = True,
-        prevent_commit: bool = False,
+            self,
+            values: dict,
+            fail_on_unknown_attr: bool = True,
+            prevent_commit: bool = False,
     ) -> t.Self:
         for key, value in values.items():
             if hasattr(self, key):
@@ -540,12 +363,12 @@ class UtilityMixin:
     # Delete
     @classmethod
     def um_delete(
-        cls,
-        pkv: int = None,
-        fields: dict = None,
-        fail_on_unknown_attr: bool = True,
-        prevent_commit: bool = False,
-        custom_delete: Delete = None,
+            cls,
+            pkv: int = None,
+            fields: dict = None,
+            fail_on_unknown_attr: bool = True,
+            prevent_commit: bool = False,
+            custom_delete: Delete = None,
     ) -> None:
         """
         pkv is the primary key value
@@ -579,34 +402,34 @@ class UtilityMixin:
 
     @classmethod
     def um_as_jsonable_dict(
-        cls,
-        execute: t.Union[Select, Insert, Update, Delete, Result, Pagination],
-        return_key_name: str = None,
-        remove_return_key: bool = False,
-        include_joins: list[tuple[str, str] | str] = None,
-        cast_joins: list[tuple[str, str]] = None,
-        all_columns_but: list = None,
-        only_columns: list = None,
-        one_or_none: bool = False,
-        first: bool = False,
-        paginate: bool = False,
-        paginate_page: int = 1,
-        paginate_per_page: int = 10,
-        paginate_count: bool = True,
+            cls,
+            execute: t.Union[Select, Insert, Update, Delete, Result, Pagination],
+            return_key_name: str = None,
+            remove_return_key: bool = False,
+            include_joins: list[tuple[str, str] | str] = None,
+            cast_joins: list[tuple[str, str]] = None,
+            all_columns_but: list = None,
+            only_columns: list = None,
+            one_or_none: bool = False,
+            first: bool = False,
+            paginate: bool = False,
+            paginate_page: int = 1,
+            paginate_per_page: int = 10,
+            paginate_count: bool = True,
     ) -> (
-        dict
-        | dict[str | None, dict]
-        | dict[t.Any, t.Any]
-        | list[dict]
-        | dict[str | None, list[dict]]
+            dict
+            | dict[str | None, dict]
+            | dict[t.Any, t.Any]
+            | list[dict]
+            | dict[str | None, list[dict]]
     ):
         cls._um_check_fsa_exists()
 
         if (
-            isinstance(execute, Select)
-            or isinstance(execute, Insert)
-            or isinstance(execute, Update)
-            or isinstance(execute, Delete)
+                isinstance(execute, Select)
+                or isinstance(execute, Insert)
+                or isinstance(execute, Update)
+                or isinstance(execute, Delete)
         ):
             if paginate:
                 execute: t.Union[Result, Pagination] = cls.__fsa__.paginate(
@@ -702,3 +525,180 @@ class UtilityMixin:
             if paginate
             else [cls._parse_rows(x, **shrink_args) for x in execute.scalars().all()],
         }
+
+    @classmethod
+    def _um_check_fsa_exists(cls):
+        if not hasattr(cls, "__fsa__"):
+            raise AttributeError(
+                dedent(
+                    """
+                The class using this mixin must have a __fsa__ attribute set to the Flask-SQLAlchemy instance",
+                Example:
+                from app.extensions import db
+                class Example(db.Model, UtilityMixin):
+                    __fsa__ = db
+                """
+                ),
+            )
+
+    @staticmethod
+    def _um_row_as_dict(row: Row) -> dict:
+        return {key: row.__dict__[key] for key in row.__dict__ if key[0] != "_"}
+
+    @staticmethod
+    def _um_parse_value(key, value, type_):
+        """
+        Returns value as type_ if possible, otherwise raises ParseValueException
+
+        :param key:
+        :param value:
+        :param type_:
+        :return:
+        """
+
+        if isinstance(type_, sqltypes.DateTime):
+            if isinstance(value, dt):
+                return value
+
+            if isinstance(value, d):
+                return dt.combine(value, dt.min.time())
+
+            if isinstance(value, str):
+                # Guess the format
+                try:
+                    return dt.strptime(value, "%Y-%m-%d")
+                except ValueError:
+                    pass
+
+                try:
+                    return dt.strptime(value, "%Y-%m-%dT%H:%M:%S")
+                except ValueError:
+                    pass
+
+                try:
+                    return dt.strptime(value, "%Y-%m-%dT%H:%M:%d.%f")
+                except ValueError:
+                    pass
+
+                raise ParseValueError(key, value, type_)
+
+        if isinstance(type_, sqltypes.Integer):
+            try:
+                return int(value)
+            except ValueError:
+                pass
+
+            raise ParseValueError(key, value, type_)
+
+        if isinstance(type_, sqltypes.Boolean):
+            if isinstance(value, bool):
+                return value
+            if isinstance(value, str):
+                if value.lower() in ["yes", "true", "t", "1"]:
+                    return True
+                return False
+            if isinstance(value, int):
+                if value == 1:
+                    return True
+                return False
+
+            raise ParseValueError(key, value, type_)
+
+        return value
+
+    @classmethod
+    def _parse_rows(
+            cls,
+            rows: t.Union[Row, list, dict],
+            include_joins: list[tuple[str, str] | str] = None,
+            cast_joins: list[tuple[str, str]] = None,
+            all_columns_but: list = None,
+            only_columns: list = None,
+            _is_join: bool = False,
+    ) -> t.Union[dict, list]:
+        """
+        Parses the rows into a dictionary or list of dictionaries
+
+        include_joins: [('given_name', 'join_attr') | 'join_attr']
+        cast_joins: [('given_name', 'join_attr.attr_of_join')]
+
+        all_columns_but: ['column_name', ...] # exclude these columns in the return
+        only_columns: ['column_name', ...] # only include these columns in the return
+
+        _is_join: Tell the function if it is parsing a join or not, this is for
+        internal use only
+        """
+
+        if isinstance(rows, list):
+            return [cls._parse_rows(row, _is_join=True) for row in rows]
+
+        if include_joins is None:
+            include_joins = []
+
+        if all_columns_but is None:
+            all_columns_but = []
+
+        if only_columns is None:
+            only_columns = []
+
+        def include_column(column_: str) -> bool:
+            if only_columns:
+                if column_ not in only_columns:
+                    return False
+
+            if all_columns_but:
+                if column_ in all_columns_but:
+                    return False
+
+            return True
+
+        data = dict()
+        for column, value in cls._um_row_as_dict(rows).items():
+            if not _is_join:
+                if not include_column(column):
+                    continue
+
+            if isinstance(value, collections.InstrumentedList):
+                continue
+
+            data[column] = value
+
+        if _is_join:
+            return data
+
+        joins = dict()
+        if include_joins:
+            for join in include_joins:
+                if isinstance(join, tuple):
+                    name, join_attr = join
+                    if hasattr(rows, join_attr):
+                        joins[name] = [
+                                          cls._parse_rows(row, _is_join=True)
+                                          for row in getattr(rows, join_attr)
+                                      ] or []
+
+                if isinstance(join, str):
+                    if hasattr(rows, join):
+                        if isinstance(getattr(rows, join), list):
+                            joins[join] = [
+                                              cls._parse_rows(row, _is_join=True)
+                                              for row in getattr(rows, join)
+                                          ] or []
+
+                            continue
+
+                        joins[join] = cls._parse_rows(rows, _is_join=True)
+
+        if cast_joins:
+            for given_name, join_cast in cast_joins:
+                split_join_cast = join_cast.split(".")
+                if hasattr(rows, split_join_cast[0]):
+                    join_attr = getattr(rows, split_join_cast[0])
+                    if hasattr(join_attr, split_join_cast[1]):
+                        data[given_name] = getattr(join_attr, split_join_cast[1])
+                    else:
+                        data[given_name] = None
+                else:
+                    data[given_name] = None
+
+        return {**data, **joins}
